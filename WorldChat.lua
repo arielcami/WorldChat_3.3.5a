1e54eab22a315f4a688cf568e2c8b457c3b436e4
arielcami <71854140+arielcami@users.noreply.github.com>
2022-08-18 18:18:21 -0500

    Añadido sistema anti-Spam v1.1
    Se agrega una función para evitar que se haga Spam, trabaja con la DB automáticamente.

--- WorldChat.lua
+++ WorldChat.lua
@@ -1,38 +1,72 @@
 
--- ES MUY FÁCIL HACER SPAM CON ESTE SCRIPT, ASÍ QUE ASEGÚRATE DE PONER LAS REGLAS DE USO BIEN CLARAS --
+local TIME = 6 	--> Coloca los SEGUNDOS de espera para usar el WorldChat nuevamente.
 
 local b = '|CFF2DEDCA' --> Color del texto y los marcos.
 
 local function Chat (E,P,M)
-	if M:find("#")==1 then  local A = P:IsAlliance() 
-		local function C()   local c=P:GetClassMask()
-			if c==1 then return "C69B6D" elseif c==2 then return "F48CBA"
-			elseif c==4 then return "AAD372" elseif c==8 then return "FFF468"
-			elseif c==16 then return "FFFFFF" elseif c==32 then return "C41E3A"
-			elseif c==64 then return "0070DD" elseif c==128 then return "3FC7EB"
-			elseif c==256 then return "8788EE" elseif c==1024 then return "FF7C0A"
+
+	if M:find("#")==1 then
+
+		local Q = CharDBQuery("SELECT `off_on` FROM `aa_worldchat` WHERE `PlayerGUID` = "..P:GetGUIDLow().."")
+
+		if Q:GetInt8(0) == 0 then 
+
+			local function C()   local c=P:GetClassMask()
+				if c==1 then return "C69B6D" elseif c==2 then return "F48CBA"
+				elseif c==4 then return "AAD372" elseif c==8 then return "FFF468"
+				elseif c==16 then return "FFFFFF" elseif c==32 then return "C41E3A"
+				elseif c==64 then return "0070DD" elseif c==128 then return "3FC7EB"
+				elseif c==256 then return "8788EE" elseif c==1024 then return "FF7C0A"
+				end
 			end
-		end
 
-		local forma = "%s"..b.."[|r|Hplayer:%s|h|CFF"..C().."%s|r"..b.."]: %s"		
+			local forma = "%s"..b.."[|r|Hplayer:%s|h|CFF"..C().."%s|r"..b.."]: %s"
 
-		if A==true then 
-			i="|TInterface\\TargetingFrame\\UI-PVP-ALLIANCE:16:16:0:0:64:64:0:32:0:38|t"
+			local A = P:IsAlliance()	
+
+			if A==true then 
+				i="|TInterface\\TargetingFrame\\UI-PVP-ALLIANCE:16:16:0:0:64:64:0:32:0:38|t"
+			else
+				i="|TInterface\\TargetingFrame\\UI-PVP-HORDE:16:16:0:0:64:64:0:38:0:36|t"
+			end
+
+			M = M:gsub("#","",1)
+			M = forma:format(i, P:GetName(), P:GetName(), M)
+			SendWorldMessage(M)
+
+			CharDBExecute("UPDATE `aa_worldchat` SET `off_on` = 1 WHERE `PlayerGUID` = "..P:GetGUIDLow().."")
+
+				local function count(ev, del, rep, PLAYER) 
+					CharDBExecute("UPDATE `aa_worldchat` SET `off_on` = 0 WHERE `PlayerGUID` = "..PLAYER:GetGUIDLow().."")
+				end
+				local seg = (TIME*1000)
+				P:RegisterEvent(count, seg, 1)
+
+			return false
 		else
-			i="|TInterface\\TargetingFrame\\UI-PVP-HORDE:16:16:0:0:64:64:0:38:0:36|t"
+			P:SendBroadcastMessage('|cff98b9d9Debes esperar unos segundos para usar el WorldChat otra vez.') return false
 		end
 
-		M = M:gsub("#","",1)
-		M = forma:format(i, P:GetName(), P:GetName(), M)
-		SendWorldMessage(M)
-		return false
-	end	
+	else
+		return
+	end
 end 
 
-local function Log(E,P) 
+local function LogIn(E,P)
+
+	CharDBExecute("INSERT IGNORE INTO `aa_worldchat` (`PlayerGUID`,`off_on`) VALUES ("..P:GetGUIDLow()..", 0)")
+
 	local function Timed(ev,de,re,w) 
-		w:SendBroadcastMessage('|cff98b9d9Para usar el WorldChat escribe "#" antes de tu mensaje.')
+		w:SendBroadcastMessage('|cff98b9d9Para usar el WorldChat escribe # antes de tu mensaje.')
 	end
 	P:RegisterEvent(Timed,2000,1)
-end
-RegisterPlayerEvent(18,Chat)   RegisterPlayerEvent(3,Log)
+end 
+
+local function LogOut(E,P) CharDBExecute("DELETE FROM `aa_worldchat` WHERE `PlayerGUID` = "..P:GetGUIDLow().."") end
+
+local function ElunaReload(E) CharDBExecute("CREATE TABLE IF NOT EXISTS `aa_worldchat` (`PlayerGUID` INT(20) NOT NULL UNIQUE, `off_on` INT(1) NOT NULL)") end
+
+RegisterPlayerEvent(18, Chat)
+RegisterPlayerEvent(3, LogIn)  
+RegisterPlayerEvent(4, LogOut)   
+RegisterServerEvent(33, ElunaReload)
